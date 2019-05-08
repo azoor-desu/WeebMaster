f9b22d561731428d7fd05a2178a5d7531fb1cbb9
Azoor <31021840+azoor-guy@users.noreply.github.com>
2019-05-08 23:56:49 +0800

    Time to do the game loop

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,30 +8,80 @@ public class GameController : MonoBehaviour {
 	public enum GameMode { Hiragana, Katakana, Both }
 	[HideInInspector] public GameMode currentGameMode;
 
-	public Text score;
-	public Text word;
+	bool gameRunning;
+
+	public Text scoreText;
+	public Text charText;
+	public Text answerText;
+	public Text answerRomanjiText;
 	public InputField inputField;
 
-	public float fieldReselectDelay = 0.05f;
+	public float fieldReselectDelay = 0.1f;
 	Coroutine coroutineSubmit;
+	Coroutine coroutineGame;
+
+	//Array of probability to select each character. Higher is more likely.
+	float[] hiragArray;
+	float[] kataArray;
+
+	int charIndex = 0;
+	int score = 0;
+	float timeStart;
 
 	void Awake() {
 
 	}
 
-	void Update() {
-		
+	void StartGame() {
+		gameRunning = true;
+		StartCoroutine(ScoreUpdate());
+		PopulateRandomList();
+		charIndex = 0;
+		score = 0;
+		timeStart = Time.time;
+		if (coroutineGame != null) {
+			StopCoroutine(coroutineGame);
+		}
+		coroutineGame = StartCoroutine(GameSeq());
 	}
 
-	private void OnEnable() {
-		inputField.text = "";
-		coroutineSubmit = null;
-		inputField.Select();
-		inputField.ActivateInputField();
+	void PopulateRandomList() {
+		CharGroup hiragGrp = MenuController._singleton.hiragGroup;
+		CharGroup kataGrp = MenuController._singleton.kataGroup;
+		hiragArray = new float[hiragArray.Length];
+		kataArray = new float[kataArray.Length];
+
+		//populate list with the latest data
+		for (int i = 0; i < hiragGrp.character.Length; i++) {
+			//Get probability by totalAttempts / correct attempts. 100% correct = 1, 50% correct = 2 etc.
+			if (hiragGrp.timesCorrect[i] == 0) { //prevent divide by zero error. needs a sufficiently large number.
+				hiragArray[i] = 10;
+			} else {
+				hiragArray[i] = hiragGrp.timesAttempted[i] / hiragGrp.timesCorrect[i];
+			}
+		}
+
+		for (int i = 0; i < kataGrp.character.Length; i++) {
+			//Get probability by totalAttempts / correct attempts. 100% correct = 1, 50% correct = 2 etc.
+			if (kataGrp.timesCorrect[i] == 0) { //prevent divide by zero error. needs a sufficiently large number.
+				kataArray[i] = 10;
+			} else {
+				kataArray[i] = kataGrp.timesAttempted[i] / kataGrp.timesCorrect[i];
+			}
+		}
 	}
 
-	private void OnDisable() {
+	void DisplayScoreText() {
+		float timer = Time.time - timeStart;
+		int minutes = Mathf.FloorToInt(timer / 60F);
+		int seconds = Mathf.FloorToInt(timer - minutes * 60);
+		string niceTime = string.Format("{0:0}:{1:00}",minutes,seconds);
+		int totalqns = MenuController._singleton.wordsPerGame;
+		scoreText.text = "Word: " + charIndex + "/" + totalqns + "\nScore: " + score + "/" + totalqns + "\nTime: " + niceTime;
+	}
 
+	void SaveProgress() {
+		print("Savegame");
 	}
 
 	public void SubmitAnswer() {
@@ -48,4 +99,68 @@ public class GameController : MonoBehaviour {
 		coroutineSubmit = null;
 	}
 
+	IEnumerator ScoreUpdate() {
+		DisplayScoreText();
+		while (gameRunning) {
+			yield return new WaitForSeconds(.5f);
+			DisplayScoreText();
+		}
+	}
+
+	string submittedAns;
+	IEnumerator GameSeq() {
+		submittedAns = "";
+		charIndex = 0;
+		while (gameRunning) {
+			//Pull a random character.
+			//If in BOTH mode, pull hiragana on evens, and kat on odds.
+
+			yield return new WaitUntil(() => submittedAns != "");
+
+			yield return null;
+		}
+	}
+
+	//int PullRandomIndex(bool hirag) {
+
+	//}
+
+	private void OnApplicationFocus(bool focus) {
+		if (!focus) {
+			SaveProgress();
+		}
+	}
+
+	private void OnEnable() {
+		inputField.text = "";
+		coroutineSubmit = null;
+		inputField.Select();
+		inputField.ActivateInputField();
+
+		StartGame();
+	}
+
+	private void OnDisable() {
+		SaveProgress();
+		gameRunning = false;
+		if (coroutineGame != null) {
+			StopCoroutine(coroutineGame);
+		}
+	}
+
+}
+
+public class CharGroup {
+
+	public string[] character;
+	public string[] romanji;
+	public int[] timesAttempted;
+	public int[] timesCorrect;
+
+	public CharGroup(int size) {
+		character = new string[size];
+		romanji = new string[size];
+		timesAttempted = new int[size];
+		timesCorrect = new int[size];
+	}
 }
--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class MenuController : MonoBehaviour {
@@ -12,7 +13,14 @@ public class MenuController : MonoBehaviour {
 	public static MenuController _singleton;
 	GameController gameCtrl;
 
+	public CharGroup hiragGroup;
+	public CharGroup kataGroup;
+
+	public int wordsPerGame = 20;
+
 	void Awake() {
+		Application.targetFrameRate = 24; //for the best cinematic experience
+
 		if (_singleton == null) {
 			 _singleton = this;
 		}
@@ -20,10 +28,52 @@ public class MenuController : MonoBehaviour {
 		gameCtrl = gameScreen.GetComponent<GameController>();
 
 		ChangeScreen(mainMenu);
+
+		//Check for files, create if not found.
+		LoadSaveFile("hiragana");
+		//LoadSaveFile("katakana");
+		Debug.LogWarning("Pls Enable Katakana Filecheck");
 	}
 
-	void Update() {
+	//Checks if save file exists. if not, create one fresh file.
+	void LoadSaveFile(string fileName) {
+		if (fileName == "hiragana") {
+			if (!System.IO.File.Exists(Application.persistentDataPath + "/" + fileName + ".json")) {
+				hiragGroup = LoadBlankFile((Resources.Load(fileName) as TextAsset).ToString());
+				string tosave = JsonUtility.ToJson(hiragGroup);
+				System.IO.File.WriteAllText(Application.persistentDataPath + "/" + fileName + ".json",tosave);
+			} else {
+				hiragGroup = JsonUtility.FromJson<CharGroup>(System.IO.File.ReadAllText(Application.persistentDataPath + "/" + fileName + ".json"));
+			}
+		} else {
+			if (!System.IO.File.Exists(Application.persistentDataPath + "/" + fileName + ".json")) {
+				kataGroup = LoadBlankFile((Resources.Load(fileName) as TextAsset).ToString());
+				string tosave = JsonUtility.ToJson(kataGroup);
+				System.IO.File.WriteAllText(Application.persistentDataPath + "/" + fileName + ".json",tosave);
+			} else {
+				kataGroup = JsonUtility.FromJson<CharGroup>(System.IO.File.ReadAllText(Application.persistentDataPath + "/" + fileName + ".json"));
+			}
+		}
+	}
 
+	CharGroup LoadBlankFile(string rawInput) {
+		string[] rawInputArray = rawInput.Split('\n');
+		List<string> rawInputList = new List<string>();
+		foreach (string item in rawInputArray) {
+			if (item.Length > 0 && item != "\r") { // Remove all empty lines
+				rawInputList.Add(item.Trim((char)13)); //remove stupid invisible characters
+			}
+		}
+
+		//Put characters and romanji into corresponding vars.
+		CharGroup chrgrp = new CharGroup(rawInputList.Count);
+		for (int i = 0; i < rawInputList.Count; i++) {
+			string[] cells = rawInputList[i].Split(';');
+			chrgrp.character[i] = cells[0];
+			chrgrp.romanji[i] = cells[1];
+		}
+		
+		return chrgrp;
 	}
 
 	void ChangeScreen(RectTransform newScreen) {
