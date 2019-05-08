980005b58183d7146991ee7ed2ad5a6078155aed
Azoor <31021840+azoor-guy@users.noreply.github.com>
2019-05-08 17:53:17 +0800

    Initial commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+	public enum GameMode { Hiragana, Katakana, Both }
+	[HideInInspector] public GameMode currentGameMode;
+
+	public Text score;
+	public Text word;
+	public InputField inputField;
+
+	public float fieldReselectDelay = 0.05f;
+	Coroutine coroutineSubmit;
+
+	void Awake() {
+
+	}
+
+	void Update() {
+		
+	}
+
+	private void OnEnable() {
+		inputField.text = "";
+		coroutineSubmit = null;
+		inputField.Select();
+		inputField.ActivateInputField();
+	}
+
+	private void OnDisable() {
+
+	}
+
+	public void SubmitAnswer() {
+		if (coroutineSubmit == null && inputField.text != "") {
+			print("ANSWER SUBMITTED");
+			inputField.text = "";
+			coroutineSubmit = StartCoroutine(SubmitAnswerCoroutine());
+		}
+	}
+
+	IEnumerator SubmitAnswerCoroutine() {
+		yield return new WaitForSecondsRealtime(fieldReselectDelay);
+		inputField.ActivateInputField();
+		inputField.Select();
+		coroutineSubmit = null;
+	}
+
+}
---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour {
+
+	public RectTransform mainMenu;
+	public RectTransform chooseMenu;
+	public RectTransform settingsMenu;
+	public RectTransform gameScreen;
+	public RectTransform chartScreen;
+
+	public static MenuController _singleton;
+	GameController gameCtrl;
+
+	void Awake() {
+		if (_singleton == null) {
+			 _singleton = this;
+		}
+
+		gameCtrl = gameScreen.GetComponent<GameController>();
+
+		ChangeScreen(mainMenu);
+	}
+
+	void Update() {
+
+	}
+
+	void ChangeScreen(RectTransform newScreen) {
+		if (mainMenu != null) {
+			mainMenu.gameObject.SetActive(false);
+		}
+		if (chooseMenu != null) {
+			chooseMenu.gameObject.SetActive(false);
+		}
+		if (settingsMenu != null) {
+			settingsMenu.gameObject.SetActive(false);
+		}
+		if (gameScreen != null) {
+			gameScreen.gameObject.SetActive(false);
+		}
+		if (chartScreen != null) {
+			chartScreen.gameObject.SetActive(false);
+		}
+
+		newScreen.gameObject.SetActive(true);
+	}
+
+	#region buttons
+	public void ButtStart() {
+		ChangeScreen(chooseMenu);
+		//buttback delegate to set to mainmenu
+	}
+
+	public void ButtHirag() {
+		ChangeScreen(gameScreen);
+		gameCtrl.currentGameMode = GameController.GameMode.Hiragana;
+	}
+
+	public void ButtBack() {
+		ChangeScreen(mainMenu);
+	}
+	#endregion
+
+}
